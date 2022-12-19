257820566fd241c62e8c512f527bd1b1991e9601
Yasith Nawodya <yasithnawodya8@gmail.com>
2022-12-19 22:38:44 +0530

    Update GuessingGame.java

--- guess name/src/guessinggame/GuessingGame.java
+++ guess name/src/guessinggame/GuessingGame.java
@@ -1,4 +1,5 @@
 package guessinggame;
+
 import javax.swing.*;
 
 public class GuessingGame {
@@ -6,35 +7,26 @@ public class GuessingGame {
 public static void main(String[] args) {
 
 int computerNumber = (int) (Math.random()*100 + 1);
-
 int userAnswer = 0;
-
 System.out.println("The correct guess would be " + computerNumber);
 
 int count = 1;
-
 while (userAnswer != computerNumber)
-
 {
-
 String response = JOptionPane.showInputDialog(null,
-
 "Enter a guess between 1 and 100", "Guessing Game", 3);
 
 userAnswer = Integer.parseInt(response);
-
 JOptionPane.showMessageDialog(null, ""+ determineGuess(userAnswer, computerNumber, count));
 
 count++;
-
 }
-
 }
 
 public static String determineGuess(int userAnswer, int computerNumber, int count){
 
-if (userAnswer <=0 || userAnswer >100) {
 
+if (userAnswer <=0 || userAnswer >100) {
 return"Your guess is invalid";
 
 }
