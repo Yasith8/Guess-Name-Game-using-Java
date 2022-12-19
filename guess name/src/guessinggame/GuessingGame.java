d6a2a199efa47d0a78b792844479fd6dbdf9cb4d
Yasith Nawodya <yasithnawodya8@gmail.com>
2022-12-19 22:39:26 +0530

    Update GuessingGame.java

--- guess name/src/guessinggame/GuessingGame.java
+++ guess name/src/guessinggame/GuessingGame.java
@@ -13,9 +13,11 @@ int userAnswer = 0;
 System.out.println("The correct guess would be " + computerNumber);
 
 int count = 1;
+  
 while (userAnswer != computerNumber)
 {
 String response = JOptionPane.showInputDialog(null,
+                                              
 "Enter a guess between 1 and 100", "Guessing Game", 3);
 
 userAnswer = Integer.parseInt(response);
